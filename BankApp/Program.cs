7cca77e6a9ca9de8a667a4af2e1db7af91e7489a
chima <chimaemekaiheonu@gmail.com>
2023-12-27 20:42:27 +0100

    new changes

---
+++ BankApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BankApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
